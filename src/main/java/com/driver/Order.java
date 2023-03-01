eda6177f1bc5c91f5a669fbfe4bd1d33bd86d058
Md Rizwan <68804148+Beingriz@users.noreply.github.com>
2023-03-01 21:21:32 +0530

    Working

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,11 +9,30 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+        int H1 = deliveryTime.charAt(0)-'0';
+        int H2 = deliveryTime.charAt(1)-'0';
+        int HH = (H1+H2)*60;
+        int M1 = deliveryTime.charAt(3)-'0';
+        int M2 = deliveryTime.charAt(4)-'0';
+        this.deliveryTime =  HH+(M1+M2);
     }
+
+
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
 }
---
+++ src/main/java/com/driver/OrderRepository.java
@@ -0,0 +1,89 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Repository
+public class OrderRepository {
+    // Creating Order Database Using Order ID
+    HashMap<String, Order> order_db = new HashMap<>();
+
+    // Creating Delivery Partner Database with Partner Id
+    HashMap<String, DeliveryPartner> del_part_db = new HashMap<>();
+
+    // Creating Pari Database
+    HashMap<String, ArrayList<String>> pair_db = new HashMap<>();
+
+    // Adding New Order
+    public void addOrder(Order order){
+        order_db.put(order.getId(), order);
+    }
+
+    // Addning new Delivery Partner
+    public void addPartner(String partnerId){
+        DeliveryPartner deliveryPartner = new DeliveryPartner(partnerId);
+        del_part_db.put(deliveryPartner.getId(),deliveryPartner);
+    }
+
+    //Adding pair of Order and Delivery Partner by their ID
+    public void addOrderPartnerPair(String orderId, String partnerId){
+        if(order_db.containsKey(orderId) && del_part_db.containsKey(partnerId)){
+            ArrayList<String>  orders = new ArrayList();
+            if(pair_db.containsKey(partnerId)){
+                orders = pair_db.get(partnerId);
+            }
+            orders.add(orderId);
+            pair_db.put(partnerId,orders);
+        }
+    }
+
+
+    // Fetcing Order Details by Order Id
+    public Order getOrder(String orderId){
+        return order_db.get(orderId);
+    }
+
+    //Fetching Partner by Id
+    public DeliveryPartner getPartner(String partnerId){
+        return del_part_db.get(partnerId);
+    }
+
+    // Display Count of Orders by Partner Id
+    public int countOrdersbyPartner(String partnerId){
+        ArrayList<String> orders = new ArrayList<>();
+        if(pair_db.containsKey(partnerId)){
+            orders = new ArrayList<>(pair_db.get(partnerId));
+        }
+        return orders.size();
+    }
+
+    //Displaying List of Orders by Partner Id
+    public ArrayList<String> ordersByPartnerID(String partnerId){
+        ArrayList<String> orders = new ArrayList<>();
+        if(pair_db.containsKey(partnerId)){
+            orders = pair_db.get(partnerId);
+        }
+        return orders;
+    }
+
+    // Displaying List of Orders
+    public ArrayList<String> allOrders(){
+        ArrayList<String> orders = new ArrayList<>();
+        for (String order: order_db.keySet()) {
+            orders.add(order);
+        }
+        return orders;
+    }
+
+    // Display count of Unassigned Orders
+    public int unAssignedOrders(){
+        int allOrders = order_db.size();
+        int assigned = 0;
+        for (ArrayList<String> order: pair_db.values()) {
+            assigned+=order.size();
+        }
+        return allOrders-assigned;
+    }
+}
